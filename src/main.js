e1aa3f295fca07a350957a4afe5133ea7495a30f
imjustpassby <260809970@qq.com>
2019-08-21 21:37:03 +0800

    优化加载，gzip

--- src/main.js
+++ src/main.js
@@ -6,17 +6,44 @@ import router from './router'
 import store from './store'
 
 import 'normalize.css/normalize.css'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
 import '@/style/index.scss'
 
 import '@/permission.js'
 
 import VueLazyLoad from 'vue-lazyload'
-
+import {
+  Button, Icon, Row, Col, Skeleton, Pagination, Table,
+  Input, Avatar, Carousel, Comment, List, Popover, Tag,
+  Tooltip, Drawer, Message, Modal, Spin, BackTop, Divider,
+  Form, Progress, Menu, Dropdown, Tabs
+} from 'ant-design-vue';
+Vue.use(Button);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Skeleton);
+Vue.use(Pagination);
+Vue.use(Table);
+Vue.use(Input);
+Vue.use(Carousel);
+Vue.use(Comment);
+Vue.use(Avatar);
+Vue.use(List);
+Vue.use(Popover);
+Vue.use(Tag);
+Vue.use(Tooltip);
+Vue.use(Drawer);
+Vue.use(Modal);
+Vue.use(Spin);
+Vue.use(BackTop);
+Vue.use(Divider);
+Vue.use(Form);
+Vue.use(Progress);
+Vue.use(Menu);
+Vue.use(Dropdown);
+Vue.use(Tabs);
+Vue.prototype.$message = Message;
 Vue.config.productionTip = false
-
-Vue.use(Antd)
 
 Vue.use(VueLazyLoad, {
   error: require('../static/img/loading.gif'),
